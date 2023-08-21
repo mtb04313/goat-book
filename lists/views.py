6e30bdc9a875ab22a91e5377e66495870e8cf6ae
mtb04313 <123456789+mtb04313@users.noreply.github.com>
2023-08-21 22:10:15 +0800

    New URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -15,8 +15,15 @@ def home_page(request):
         #item.text = request.POST["item_text"]
         #item.save()
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+        #return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
     
+    #items = Item.objects.all()
+    #return render(request, "home.html", {"items": items})
+    return render(request, "home.html")
+    
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+    return render(request, "list.html", {"items": items})
     
