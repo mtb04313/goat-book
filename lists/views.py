911dfe9ccd27d7f04bed481eb30e22143b02edf4
mtb04313 <123456789+mtb04313@users.noreply.github.com>
2023-08-21 22:28:48 +0800

    URLs for forms x2, moved code in views + tests, new URL.

--- lists/views.py
+++ lists/views.py
@@ -10,13 +10,13 @@ def home_page(request):
         # return HttpResponse("You submitted: " + request.POST["item_text"])    
     # return render(request, "home.html")
 
-    if request.method == "POST":
-        #item = Item()
-        #item.text = request.POST["item_text"]
-        #item.save()
-        Item.objects.create(text=request.POST["item_text"])
-        #return redirect("/")
-        return redirect("/lists/the-only-list-in-the-world/")
+    # if request.method == "POST":
+        # #item = Item()
+        # #item.text = request.POST["item_text"]
+        # #item.save()
+        # Item.objects.create(text=request.POST["item_text"])
+        # #return redirect("/")
+        # return redirect("/lists/the-only-list-in-the-world/")
     
     #items = Item.objects.all()
     #return render(request, "home.html", {"items": items})
@@ -27,3 +27,6 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, "list.html", {"items": items})
     
+def new_list(request):
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect("/lists/the-only-list-in-the-world/")
