a19ffc26ce8ef8971cd87f7827861282fdb38064
mtb04313 <123456789+mtb04313@users.noreply.github.com>
2023-08-21 16:33:35 +0800

    Can only handle one list item

--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     #return HttpResponse("<html><title>To-Do lists</title></html>")
-    return render(request, "home.html")
+    
+    # if request.method == "POST":
+        # return HttpResponse("You submitted: " + request.POST["item_text"])    
+    # return render(request, "home.html")
+
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text", "")},
+    )
