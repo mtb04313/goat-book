831afd83b16a3ab1bfc575b9c778bfa4ce3fe630
mtb04313 <123456789+mtb04313@users.noreply.github.com>
2023-09-30 22:05:22 +0800

    Hacked ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-97ffdyhz9s17t(j=+lmg$xo9odsmt%4*c*cniox=_wq4nf+e#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
